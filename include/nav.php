f4ad2c8f204e1820d0d8b5325ba676e1114eb85e
Randey1988 <154453655+Randey1988@users.noreply.github.com>
2024-06-11 03:04:02 +0200

    Update nav.php

--- include/nav.php
+++ include/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary nav_color">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/www/cajuca/home.php">
-        <img src="/www/cajuca/media/img/grafismo.jpg" alt="CAJUCA" width="250" height="50">
+    <a class="navbar-brand" href="/home.php">
+        <img src="/media/img/grafismo.jpg" alt="CAJUCA" width="250" height="50">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,7 @@
       <ul class="navbar-nav nav-filling .nav-justified me-auto mb-2 mb-lg-0">
 
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/www/cajuca/home.php">Inicio</a>
+          <a class="nav-link active" aria-current="page" href="/home.php">Inicio</a>
         </li>
 
         <li class="nav-item dropdown">
@@ -19,10 +19,10 @@
             Eventos
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/www/cajuca/php/list/list_eventos.php">Ver listado</a></li>
+            <li><a class="dropdown-item" href="/php/list/list_eventos.php">Ver listado</a></li>
             <?php    
               if ($_SESSION ['permisos'] > 1){
-                echo '<li><a class="dropdown-item" href="/www/cajuca/php/add/add_eventos.php">Agregar Nuevos Eventos</a></li>';
+                echo '<li><a class="dropdown-item" href="/php/add/add_eventos.php">Agregar Nuevos Eventos</a></li>';
               }
             ?>
           </ul>
@@ -33,10 +33,10 @@
             Monitores
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/www/cajuca/php/list/list_monitores.php">Ver listado</a></li>
+            <li><a class="dropdown-item" href="/php/list/list_monitores.php">Ver listado</a></li>
             <?php    
               if ($_SESSION ['permisos'] > 1){
-                echo '<li><a class="dropdown-item" href="/www/cajuca/php/add/add_monitores.php">Agregar Nuevos Monitores</a></li>';
+                echo '<li><a class="dropdown-item" href="/php/add/add_monitores.php">Agregar Nuevos Monitores</a></li>';
               }
             ?>
           </ul>
@@ -47,10 +47,10 @@
             Clientes
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/www/cajuca/php/list/list_clientes.php">Ver listado</a></li>
+            <li><a class="dropdown-item" href="/php/list/list_clientes.php">Ver listado</a></li>
             <?php    
               if ($_SESSION ['permisos'] > 1){
-                echo '<li><a class="dropdown-item" href="/www/cajuca/php/add/add_clientes.php">Agregar Nuevos Clientes</a></li>';
+                echo '<li><a class="dropdown-item" href="/php/add/add_clientes.php">Agregar Nuevos Clientes</a></li>';
               }
             ?>
           </ul>
@@ -61,10 +61,10 @@
             Actividades
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/www/cajuca/php/list/list_actividades.php">Ver listado</a></li>
+            <li><a class="dropdown-item" href="/php/list/list_actividades.php">Ver listado</a></li>
             <?php    
               if ($_SESSION ['permisos'] > 1){
-                echo '<li><a class="dropdown-item" href="/www/cajuca/php/add/add_actividades.php">Agregar Nuevas Actividades</a></li>';
+                echo '<li><a class="dropdown-item" href="/php/add/add_actividades.php">Agregar Nuevas Actividades</a></li>';
               }
             ?>
           </ul>
@@ -75,10 +75,10 @@
             Materiales
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/www/cajuca/php/list/list_materiales.php">Ver listado</a></li>
+            <li><a class="dropdown-item" href="/php/list/list_materiales.php">Ver listado</a></li>
             <?php    
               if ($_SESSION ['permisos'] > 1){
-                echo '<li><a class="dropdown-item" href="/www/cajuca/php/add/add_materiales.php">Agregar Nuevos Materiales</a></li>';
+                echo '<li><a class="dropdown-item" href="/php/add/add_materiales.php">Agregar Nuevos Materiales</a></li>';
               }
             ?>
           </ul>
@@ -89,9 +89,9 @@
             Utilidades
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/www/cajuca/php/tools/fechas_monitores.php">Monitores en activo</a></li>
-            <li><a class="dropdown-item" href="/www/cajuca/php/tools/informe_eventos.php">Informe de eventos</a></li>
-            <li><a class="dropdown-item" href="/www/cajuca/php/tools/fechas_eventos.php">Período de eventos</a></li>
+            <li><a class="dropdown-item" href="/php/tools/fechas_monitores.php">Monitores en activo</a></li>
+            <li><a class="dropdown-item" href="/php/tools/informe_eventos.php">Informe de eventos</a></li>
+            <li><a class="dropdown-item" href="/php/tools/fechas_eventos.php">Período de eventos</a></li>
           </ul>
         </li>
 
@@ -102,11 +102,11 @@
           <ul class="dropdown-menu">
             <?php    
               if ($_SESSION ['permisos'] == 3){
-                echo '<li><a class="dropdown-item" href="/www/cajuca/php/login/login_list.php">Listado de usuarios</a></li>';
+                echo '<li><a class="dropdown-item" href="/php/login/login_list.php">Listado de usuarios</a></li>';
               }
             ?>
-            <li><a class="dropdown-item" href="/www/cajuca/php/login/login_edit.php?usuario=<?php echo $_SESSION['usuario']; ?>">Editar Datos</a></li>
-            <li><a class="dropdown-item" href="/www/cajuca/php/login/logout.php">Cerrar Sesion</a></li>
+            <li><a class="dropdown-item" href="/php/login/login_edit.php?usuario=<?php echo $_SESSION['usuario']; ?>">Editar Datos</a></li>
+            <li><a class="dropdown-item" href="/php/login/logout.php">Cerrar Sesion</a></li>
           </ul>
         </li>
 
