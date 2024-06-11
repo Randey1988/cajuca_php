958969ad0434829128c6e1c9b7c00818929e257f
Randey1988 <154453655+Randey1988@users.noreply.github.com>
2024-06-11 08:36:05 +0200

    Update session.php

--- include/session.php
+++ include/session.php
@@ -11,17 +11,18 @@ if (isset($_SESSION['usuario'])){
 
     $_SESSION['URL'] = $_SERVER['REQUEST_URI']; //Recuerda la URL actual para acciones futuras. Si se accede desde aqui a ciertas funciones
     $usuario = $_SESSION['usuario'];
+    
     $db_usuarios = mysqli_query ($conexion, "SELECT permisos, estado FROM usuarios WHERE usuario='$usuario'");     //Se consulta los datos referentes al usuario actual
 
     // Verificar si se encontraron resultados
     if (mysqli_num_rows($db_usuarios) == 0) {
-        header("Location: /php/login/logout.php?error=usuario_inexistente"); //Redirige a logout
+        header("Location: /php/login/logout.php?error=usuario_inexistente&usuario='$usuario'"); //Redirige a logout
         die;
     }else{
         $usuario_completo = mysqli_fetch_assoc($db_usuarios);
     
         if ($usuario_completo['estado'] == 6){      //Verfificar si el usuario esta deshabilitado
-            header("Location: /php/login/logout.php?error=usuario_deshabilitado"); //Redirige a logout
+            header("Location: /php/login/logout.php?error=usuario_deshabilitado&usuario='$usuario'"); //Redirige a logout
             die;
         }else{
             $_SESSION ['permisos'] = $usuario_completo['permisos'];
